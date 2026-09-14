f3f1e8a5b50910a6c584b8dd952dd71412319234
otakatikidebranding <otakatikide.branding@gmail.com>
2026-09-14 10:40:57 +0700

    Update AnnouncementCard.tsx

--- src/components/AnnouncementCard.tsx
+++ src/components/AnnouncementCard.tsx
@@ -85,7 +85,7 @@ export const AnnouncementCard: React.FC<AnnouncementCardProps> = () => {
             id="announcement-title"
             className="text-[#1E1133] font-extrabold text-xl sm:text-[25px] leading-tight tracking-tight mb-2.5 sm:mb-3.5"
           >
-            Wajah Baru Otakatikide!
+            Branding & Creative
           </h2>
 
           {/* Body Paragraphs */}
@@ -97,12 +97,12 @@ export const AnnouncementCard: React.FC<AnnouncementCardProps> = () => {
             </p>
 
             <p>
-              Buat kamu yang mau lihat portofolio,
+              Kunjungi Profil terbaru kami,
               <br />
-              cek layanan, atau mau diskusi project,
+              cek promo layanan, atau mau diskusi project,
               <br />
               <strong className="text-[#1E1133] font-bold">
-                sekarang bisa langsung akses via:
+                klik website resmi kami dibawah:
               </strong>
             </p>
           </div>
